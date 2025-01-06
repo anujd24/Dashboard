0f467c6dd6d4b6046eebd6fe4be21458abce40ab
Anuj Dubey <anujd2408@gmail.com>
2025-01-07 01:15:09 +0530

    adding

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
 
 export default function DashboardLayout({
     children,
@@ -8,13 +9,17 @@ export default function DashboardLayout({
 }>) {
     return <div className="h-screen flex">
         {/* Left */}
-        <div className="w-[14%] bg-blue-300">
-            <Link href="/">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-300 p-4 ">
+            <Link 
+                href="/"
+                className="flex items-center justify-center lg:justify-start gap-2"
+                >
                 <Image src="/logo.png" alt="logo" width={32} height={32}></Image>
-                <span></span>
+                <span className="hidden lg:block">AD</span>
             </Link>
+            <Menu/>
         </div>
         {/* Right */}
-        <div className="w-[86%] bg-red-300">r</div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-red-300">r</div>
     </div>
 }
