979ec299a15c2653c4ae451d748adb005e6cabb2
Anuj Dubey <anujd2408@gmail.com>
2025-01-07 01:08:19 +0530

    adding

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function DashboardLayout({
     children,
 }: Readonly<{
@@ -5,7 +8,12 @@ export default function DashboardLayout({
 }>) {
     return <div className="h-screen flex">
         {/* Left */}
-        <div className="w-[14%] bg-blue-300">L</div>
+        <div className="w-[14%] bg-blue-300">
+            <Link href="/">
+                <Image src="logo.png" alt="logo" width={32} height={32}></Image>
+                <span></span>
+            </Link>
+        </div>
         {/* Right */}
         <div className="w-[86%] bg-red-300">r</div>
     </div>
