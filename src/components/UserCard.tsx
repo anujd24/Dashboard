f4943d454bac72ecdc0049a0b5a3f5f0670d9ca8
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 12:52:09 +0530

    adding

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,9 @@
+const UserCard = ({type}: {type:string}) => {
+    return (
+        <div className="rounded-2xl odd:bg-adPurple even:bg-adYellow">
+            odys
+        </div>
+    )
+}
+
+export default UserCard
