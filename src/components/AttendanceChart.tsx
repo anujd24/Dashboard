c3a7e920f9d959a766e049bc83d4d95d03e4a939
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 23:58:15 +0530

    adding

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -30,8 +30,8 @@ const data = [
 const AttendanceChart = () => {
     return (
         <div className='bg-white rounded-lg p-4 h-full'>
-            <div className=''>
-                <h1>Attendance</h1>
+            <div className='flex justify-between items-center'>
+                <h1 className='text-lg font-semibold'>Attendance</h1>
                 <Image src={"/moreDark.png"} alt=""width={20} height={20}></Image>
             </div>
             <ResponsiveContainer width="100%" height="90%">
