896d133199372194d4c4ea59b2d069a521662196
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 19:46:30 +0530

    adding

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -54,7 +54,7 @@ const CountChart = () => {
         <div className='bg-white rounded-xl w-full h-full p-4'>
             {/* Title */}
             <div className='flex justify-between items-center'>
-                <h1>Tenants</h1>
+                <h1 className='text-lg font-semibold'>Tenants</h1>
                 <Image src={"/moreDark.png"} alt='' height={20} width={20}></Image>
             </div>
 
@@ -64,7 +64,19 @@ const CountChart = () => {
             </div>
 
             {/* Bottom */}
-            <div></div>
+            <div className='flex justify-center gap-16'> 
+              <div className='flex flex-col gap-1'>
+                <div className='w-5 h-5 bg-adSky rounded-full'></div>
+                <h1 className='font-bold'>1,234</h1>
+                <h2 className='text-xs text-gray-300'>Single (10%)</h2>
+              </div>
+
+              <div className='flex flex-col gap-1'>
+                <div className='w-5 h-5 bg-adSky rounded-full'></div>
+                <h1 className='font-bold'>4,567</h1>
+                <h2 className='text-xs text-gray-300'>Multiple (90%)</h2>
+              </div>
+            </div>
         </div>
     )
 }
