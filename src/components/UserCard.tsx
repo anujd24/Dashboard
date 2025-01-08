cba003fcd234e6942f20fa7bc25fc3bfda7d2d55
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 12:58:04 +0530

    adding

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,6 @@
 const UserCard = ({type}: {type:string}) => {
     return (
-        <div className="rounded-2xl odd:bg-adPurple even:bg-adYellow">
+        <div className="rounded-2xl odd:bg-adPurple even:bg-adYellow p-4 flex-1">
             odys
         </div>
     )
