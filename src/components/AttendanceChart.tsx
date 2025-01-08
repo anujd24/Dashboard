e93c52685e9eba107a0c7adda0f30c6f8340d0fa
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 23:28:05 +0530

    adding

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,5 +1,28 @@
 "use client"
 
+const data = [
+    {
+      name: 'Mon',
+      present: 40,
+      absent: 24,
+    },
+    {
+      name: 'Tues',
+      present: 30,
+      absent: 13,
+    },
+    {
+      name: 'Wed',
+      present: 20,
+      absent: 98,
+    },
+    {
+      name: 'Fri',
+      present: 27,
+      absent: 38,
+    }
+  ];
+
 const AttendanceChart = () => {
     return (
         <div>AttendanceChart</div>
