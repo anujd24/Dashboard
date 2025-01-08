6391b58768b545ef70db0b09d970f82b019a6101
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 21:00:56 +0530

    adding

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -32,19 +32,17 @@ const CountChart = () => {
             </div>
 
             {/* Chart */}
-            <div className='w-full h-[75%]'>
+            <div className='relative w-full h-[75%]'>
             <ResponsiveContainer>
             <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
-            <RadialBar
-        
-            
-            background
-            dataKey="count"
-          />
+            <RadialBar                    
+              background
+              dataKey="count"
+            />
         </RadialBarChart>
-      </ResponsiveContainer>
-    
-            </div>
+        </ResponsiveContainer>
+        <Image src={"/maleFemale.png"} alt='' height={50} width={50} className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'></Image>
+      </div>
 
             {/* Bottom */}
             <div className='flex justify-center gap-16'> 
