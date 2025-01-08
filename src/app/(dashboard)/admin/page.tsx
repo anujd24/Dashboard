ab5ad93e1f7938fb53071128bcf647e78d083706
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 13:01:47 +0530

    adding

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -3,9 +3,11 @@ import UserCard from '@/components/UserCard'
 
 function adminPage() {
   return (
-    <div>
-      <div>
-        <div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT SIDE */}
+      <div className='w-full lg:w-2/3'>
+      {/* USER CARDS */}
+        <div className='flex gap-4 justify-between flex-wrap'>
           <UserCard type='Tenant'></UserCard>
           <UserCard type='Landlord'></UserCard>
           <UserCard type='Manager'></UserCard>
@@ -13,7 +15,8 @@ function adminPage() {
         </div>
       </div>
       
-      <div>r</div>
+      {/* RIGHT SIDE */}
+      <div className='w-full lg:w-1/3'>r</div>
     </div>
   )
 }
