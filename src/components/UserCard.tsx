ba27637e61c8fc06e77e11ab64f4d501f0275958
Anuj Dubey <anujd2408@gmail.com>
2025-01-08 13:05:30 +0530

    adding

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image"
+
 const UserCard = ({type}: {type:string}) => {
     return (
         <div className="rounded-2xl odd:bg-adPurple even:bg-adYellow p-4 flex-1 min-w-[130px]">
-            odys
+            <div>
+                <span></span>
+                <Image src={"/more.png"} alt="" width={20} height={20}></Image>
+            </div>
         </div>
     )
 }
