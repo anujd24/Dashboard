21309c2868f47fb205f3a53a56ee57a9113af95e
Anuj Dubey <anujd2408@gmail.com>
2025-01-09 20:15:21 +0530

    adding

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import { useState } from "react";
+
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const EventCalendar = () => {
+    const [value, onChange] = useState<Value>(new Date());
     <div></div>
 }
 
