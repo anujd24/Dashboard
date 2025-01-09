c0042a8980e23eec9d151ec58bd4ae75cc978323
Anuj Dubey <anujd2408@gmail.com>
2025-01-09 20:24:45 +0530

    adding

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -3,6 +3,7 @@ import UserCard from '@/components/UserCard'
 import CountChart from '@/components/CountChart'
 import AttendanceChart from '@/components/AttendanceChart'
 import FinanceChart from '@/components/FinanceChart'
+import EventCalendar from '@/components/EventCalendar'
 
 function adminPage() {
   return (
@@ -34,7 +35,8 @@ function adminPage() {
       </div>
       
       {/* RIGHT SIDE */}
-      <div className='w-full lg:w-1/3 flex flex-col gap-8'></div>
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+      </div>
     </div>
   )
 }
