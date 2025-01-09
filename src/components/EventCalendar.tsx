2240d34b18a1245206d7256280d6cf30e16dfd44
Anuj Dubey <anujd2408@gmail.com>
2025-01-09 20:46:57 +0530

    adding

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Calendar from "react-calendar";
 
 
+
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -12,7 +13,7 @@ const EventCalendar = () => {
     const [value, onChange] = useState<Value>(new Date());
 
    return (
-    <div> <Calendar onChange={onChange} value={value} /></div>
+    <div className="bg-white p-4 rounded-md"> <Calendar onChange={onChange} value={value} /></div>
 
    ) 
 }
