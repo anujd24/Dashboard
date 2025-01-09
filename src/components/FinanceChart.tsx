3d2302f5a9250a1ef848cd4a576f2bb409baf67c
Anuj Dubey <anujd2408@gmail.com>
2025-01-09 12:33:48 +0530

    adding

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
     {
